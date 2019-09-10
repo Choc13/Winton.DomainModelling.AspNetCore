87949c8f6b35432119173034d5901cb54d526b84
Choc13 <matthew.thornton13@gmail.com>
2019-09-10 18:14:52 +0100

    Convert validation results to HTTP responses.

---
+++ src/TestApi/Controllers/ValuesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Winton.DomainModelling;
+using Winton.DomainModelling.AspNetCore;
+
+namespace TestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ValueDto>> Get()
+        {
+            return new ActionResult<IEnumerable<ValueDto>>(Enumerable.Empty<ValueDto>());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ValueDto> Get(int id)
+        {
+            return new ValueDto(13, "Test");
+        }
+
+        [HttpPost]
+        public ActionResult<Value> Post([FromBody] ValueDto valueDto)
+        {
+            return Value.Create(valueDto.Data, valueDto.Name).ToActionResult();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ValueDto valueDto)
+        {
+        }
+
+        public sealed class Value
+        {
+            public Value(int data, string name, bool foo)
+            {
+                Data = data;
+                Name = name;
+                Foo = foo;
+            }
+
+            public int Data { get; }
+
+            public bool Foo { get; }
+
+            public string Name { get; }
+
+            internal static Result<Value> Create(int data, string name)
+            {
+                Validation<int> ValidateData()
+                {
+                    return new Validator<int>(data)
+                        .Expect(d => d % 2 != 0, nameof(Data), "Data must be odd.")
+                        .Expect(d => d > 0, nameof(Data), "Data must be positive.")
+                        .Validate();
+                }
+
+                Validation<string> ValidateName()
+                {
+                    return new Invalid<string>(new ValidationError(nameof(Name), "Name error."));
+                }
+
+                Validation<bool> Foo()
+                {
+                    return new Invalid<bool>(
+                        new Dictionary<string, IEnumerable<string>>
+                        {
+                            { nameof(Foo), new List<string> { "Test" } }
+                        });
+                }
+
+                Func<int, string, Value> create = (d, s) => new Value(d, s, true);
+
+                // ValidateData.And(ValidateName).Then(create);
+
+                return create.Apply(ValidateData()).Apply(new Valid<string>(name)).ToResult();
+            }
+        }
+
+        public class ValueDto
+        {
+            public ValueDto(int data, string name)
+            {
+                Data = data;
+                Name = name;
+            }
+
+            [Required]
+            public int Data { get; }
+
+            [Required]
+            [MaxLength(10)]
+            public string Name { get; }
+        }
+    }
+}
---
+++ src/TestApi/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace TestApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection()
+               .UseMvc()
+               .UseSwagger()
+               .UseSwaggerUI(options => options.SwaggerEndpoint("v1/swagger.json", "v1"));
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSwaggerGen(options => options.SwaggerDoc("v1", new Info { Version = "v1" }))
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+    }
+}
